d60a0cf44ef35d9f158b430e413c6c8869330d62
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-19 21:46:31 +0200

    first lesson commit

---
+++ Lesson0/Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const float PI = 3.14F;
+
+
+// Calkowite
+sbyte zmienna7 = sbyte.MaxValue;
+byte zmienna4 = byte.MaxValue;
+short zmienna5 = short.MaxValue;
+ushort zmienna8 = ushort.MaxValue;
+long zmienna6 = long.MaxValue;
+ulong zmienna10 = ulong.MaxValue;
+int zmienna = int.MaxValue ;
+uint zmienna9 = uint.MaxValue;
+
+//Zmienno Przcinkowe
+float zmienna11 = float.MaxValue ;
+double zmienna12 = double.MaxValue;
+decimal zmienna13 = decimal.MaxValue;
+
+Console.WriteLine(zmienna);
+string zmienna2 = "Hello";
+char zmienna14 = '[';
+bool zmienna15 = 5>2 &&  5>3;
+bool zmienna16 = zmienna15;
+var zmienna17 = zmienna14;
+Console.WriteLine(PI);
+Console.ReadKey();
---
+++ Lesson1/Lesson11/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int a = 2;
+int b = 3;
+
+
+// % reszta z dzielenia 
+//a += b;// dodawanie do aktualnej wartosci a= a+b; to samo z - * / 
+
+a += 1; //a++
+a -= 1; //a--
+
+bool d = 2 >= 1;
+
+bool e = 3 == 2; //przyrownanie wartrosci
+bool f = 3 != 4;   //przyrownanie roznosci
+
+bool g = !(2 == 2); // negacja argumentu 
+
+
+int c = (a + a) * a; ;// 
+
+bool h = 1 == 1 && 1 != 1;  // I  czyli AND 
+
+bool t = 1 == 1 || 2 != 2; // LUB czyli OR
+
+bool r = true || true && false;
+bool w = (true || true) && false;
+
+Math.Pow(2, 3);
+
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine("Teraz znaki logiczne po znakach przyrownaniu wartosci ");
+
+Console.WriteLine(h);
+Console.WriteLine(t);
+Console.WriteLine(r);
+Console.WriteLine(w);
+
+Console.WriteLine("Mnozenie dzielenie z math biblioteki system");
+Console.WriteLine((Math.Pow(2, 3) + 1) * 2);
+Console.WriteLine(Math.Pow(2, 3));
+Console.WriteLine(Math.Sqrt(9));//pierwiastek
+Console.WriteLine(Math.Abs(-5));// bezwzgledna
+Console.ReadKey();
+
