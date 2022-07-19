6cc85ff521e5cd94be37f30a69c0bc0e39f27632
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-19 22:46:28 +0200

    basic c# update tables and switch

---
+++ Lesson 8 Swich/Lesson 8 Swich/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+int liczba;
+liczba = int.Parse(Console.ReadLine());
+
+//liczba
+//if (liczba == 1)
+//{
+//    Console.WriteLine("Jeden");
+//}
+//else if (liczba == 2)
+//{
+//    Console.WriteLine("Dwa");
+//}
+//else if (liczba == 3)
+//{
+//    Console.WriteLine("Trzy");
+//}
+//else
+//{
+//    Console.WriteLine("Inna wartosc");
+//}
+
+char znak = 'c';
+
+switch (znak)
+{
+    case 'a':
+        Console.WriteLine("Jeden A");
+        goto koniec;
+    case 'b':
+        Console.WriteLine("Dwa B");
+        goto case 'c';
+    case 'c':
+        Console.WriteLine("Trzy C");
+        break;
+    default:
+        Console.WriteLine("Inna wartosc");
+        break ;
+}
+
+Console.WriteLine("Dodatkowy tekst");
+koniec:
+Console.WriteLine("Koniec");
+Console.ReadKey();
---
+++ Lesson 9 Tablica Array/Lesson 9 Tablica Array/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Tablice
+
+int[] tablica = new int[365];
+string[] dniTygodnia = { "Pon", "Wtr", "Sr", "Czw", "Pt", "Sob", "Nd" };
+
+for (int i = 0; i < dniTygodnia.Length; i++)
+{
+    Console.WriteLine(dniTygodnia[i]);
+}
+
+    Console.WriteLine( );
+
+Console.ReadKey();
