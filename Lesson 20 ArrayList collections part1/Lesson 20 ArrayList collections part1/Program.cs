99bd793d0418e2a4bb16c7dfe5f8ae6a2e6ce1df
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-20 06:34:35 +0200

    Arraylist and list review c# training

---
+++ Lesson 20 ArrayList collections part1/Lesson 20 ArrayList collections part1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace Lesson_20_ArrayList_collections_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //ctrl + . library help
+            ArrayList list = new ArrayList(10);
+            //[1],[2],[3]
+            list.Add(6);
+            list.Add("Bartek");
+            list.Add(new Auto("BMW"));
+        //    Console.WriteLine(list.Count);
+            list.RemoveAt(1);
+
+          //  Console.WriteLine(list[1]);// [1] pozition list is BMW
+          //  Console.WriteLine(list.Count);
+            list.Insert(0, "Ala");
+            list.Reverse();
+         //   list.Clear();
+          //  Console.WriteLine(list.Count);
+             Console.WriteLine(list[2].ToString());
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+
+        }
+        class Auto
+        {
+            //ctor + tab konstructor tip
+
+            public string Name { get; set; }
+            public Auto(string brand)
+            {
+                Name = brand;
+            }
+            //override spacja wybierz to string i generuje sie automatycznie , nice option.
+            public override string ToString()
+            {
+                return Name;
+            }
+
+        }
+    }
+}
---
+++ Lesson 21 List collection/Lesson 21 List collection/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson_21_List_collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        //    List<int> list = new List<int>();
+            List<Auto> list = new List<Auto>();
+         //   List<Auto> list2 = new List<Auto>();
+        //    list2.Add(new Auto("Fiat"));
+          //  list2.AddRange(list);
+          //  list2.RemoveAt(4);
+            list.Add(new Auto("BMW  "));
+            list.Add(new Auto("Mercedes"));
+            list.Add(new Auto ("Ford"));
+           // Console.WriteLine(list[]);
+            foreach (Auto item in list)
+            {
+               
+                Console.WriteLine(item.Name);
+            }
+            //list.Clear();
+
+            Console.ReadKey();
+          
+        }
+        class Auto
+        {
+            public string Name { get; set; }
+            public Auto(string brand)
+            {
+            Name = brand;
+            }
+            public void TurnOnEngine()
+            {
+
+            }
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+    }
+}
