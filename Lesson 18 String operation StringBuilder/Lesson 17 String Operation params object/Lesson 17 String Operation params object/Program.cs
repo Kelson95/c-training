53a8f78370e4e73fbe2dbaca1b5b678db308a8bb
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-20 04:18:11 +0200

    String advanced operation training c#

---
+++ Lesson 18 String operation StringBuilder/Lesson 17 String Operation params object/Lesson 17 String Operation params object/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson_17_String_Operation_params_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GenerateText("My name is #0 and and I'm #1 years old", "Adam", 22));
+            
+
+            Console.ReadKey();
+        }
+        //object obj = 1;
+        //object ojb2 = "Tekst";
+        public static string GenerateText( string text, params object[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                text = text.Replace("#" + i, args[i].ToString());
+            }
+        return text;
+        }
+
+    }
+}
---
+++ Lesson 18 String operation StringBuilder/Lesson 18 String operation StringBuilder/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Lesson_18_String_operation_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //'ctrl' + '.' dodawanie biblioteki 
+
+
+
+            Console.WriteLine("Hello, World!");
+            Stopwatch stopwatch = new Stopwatch();
+            int ile = 30000;
+            stopwatch.Start();
+            BuildString(ile);
+            // Console.WriteLine(BuildString(ile));
+            stopwatch.Stop();
+            Console.WriteLine("Standard method build strings : {0} ms", stopwatch.ElapsedMilliseconds);
+
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            BuildStringBuilder(ile);
+            //  Console.WriteLine(BuildStringBuilder(ile));
+            stopwatch.Stop();
+            Console.WriteLine("Standard method build strings builder : {0}ms", stopwatch.ElapsedMilliseconds);
+
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Method building string 
+        /// </summary>
+        /// <param name="ile">Give lenght of loop</param>
+        /// <returns>Returning ready text</returns>
+
+        public static string BuildString(int ile)
+        {
+            string tekst = "";
+            for (int i = 0; i < ile; i++)
+            {
+                tekst += i;
+            }
+            return tekst;
+        }
+        public static string BuildStringBuilder(int ile)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < ile; i++)
+            {
+                stringBuilder.Append(i);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
