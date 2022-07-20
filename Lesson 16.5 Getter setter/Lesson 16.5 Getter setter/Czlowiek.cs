39b74097c105ed0972a8b47796d32e4d3d44ac5d
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-20 04:16:45 +0200

    getter and setter training c#

---
+++ Lesson 16.5 Getter setter/Lesson 16.5 Getter setter/Czlowiek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_16._5_Getter_setter
+{
+    internal class Czlowiek
+    {
+       public string name;
+         private int age;
+
+        // //prop tab tab
+        // public int MyProperty { get; set; }
+        //public int age { get; set; }
+
+        // ////propfull tab tab//
+     
+        // private int myVar;
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+        //private int myVar;
+
+        public int ageProperty
+        {
+            get { return age; }
+            set
+            { 
+                if(value >= 0 && value <= 200)
+                age = value;
+                else
+                {
+                    age = 0;
+                }
+            }
+        }
+
+
+        //public int GetAge()
+        // {
+        //     return age;
+        // }
+
+        // public void setAge(int w)
+        // {
+        //     if(w >= 0 && w<)
+        //     age = w;
+        // }
+
+    }
+}
---
+++ Lesson 16.5 Getter setter/Lesson 16.5 Getter setter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson_16._5_Getter_setter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Czlowiek adi = new Czlowiek();
+            adi.name = "Adrian";
+            //  adi.age = -20;
+            //  adi.setAge(25);
+            adi.ageProperty = 20;
+            adi.ageProperty = -20;
+            adi.ageProperty = 25;
+
+
+
+            Console.WriteLine($"Jestem {adi.name} lat {adi.ageProperty} ");
+         //   Console.WriteLine(String.Format("Jestem {0} lat {1}", adi.name, adi.GetAge()));
+            Console.ReadKey();
+        }
+    }
+}
