f8f840ca41bea9a87df68e2f33f6a2ae4c44c104
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-20 04:59:46 +0200

    Culture info training

---
+++ Lesson 19 Culture Info/Lesson 19 Culture Info/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+namespace Lesson_19_Culture_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello, World!");
+            var c = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            //foreach (var item in c)
+            //{
+            //    Console.Write(item.Name);
+            //    Console.WriteLine(item.NumberFormat.NumberDecimalSeparator);
+            //}
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            //     CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator = ":"; //only for read
+            var newCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();// rzutowanie co to ?
+            newCulture.NumberFormat.NumberDecimalSeparator = ":";
+            newCulture.NumberFormat.NegativeSign = "MINUS ";
+            CultureInfo.CurrentCulture = newCulture;
+
+            int a = -5;
+            float b = 3.14f;
+            Console.WriteLine($"{a}\n{b}");
+
+            Console.WriteLine(" Pierwsza liczba " + a+"\n" +" Druga liczba "+  b);
+
+            Console.ReadKey();
+
+        }
+    }
+}
