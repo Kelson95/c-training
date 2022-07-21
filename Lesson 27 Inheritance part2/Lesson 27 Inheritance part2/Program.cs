04523e445375cf7fc3c8d85f5b189eb4559f9e93
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-21 02:59:33 +0200

    inheritance another example training c#

---
+++ Lesson 27 Inheritance part2/Lesson 27 Inheritance part2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Lesson_27_Inheritance_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Character p1 = new Character("Dwarf", 200);
+            Mage m1 = new Mage("Elf", 150, 300);
+            Console.WriteLine(p1.name +" "+ p1.HealthPoints);
+            Console.WriteLine(m1.name + " "+ m1.HealthPoints + " " + m1.mana);
+            Console.ReadKey();
+        }
+    }
+    class Character
+    {
+        //ctor + tab tab  utworzeniekonstruktora .
+        //public Character()
+        //{
+
+        //}
+        public Character(string name, int HealthPoints)
+        {
+            this.name = name;
+            this.HealthPoints = HealthPoints;
+        }
+        public string name;
+        public int HealthPoints;
+
+    }
+    class Mage : Character
+    {
+        public Mage(string name,int HealthPoints, int mana) /*:base(name,HealthPoints)*/ :base("Elf" , 150)
+            //base metoda by zdobyc zmienne dziedziczone po wypisaniu 
+        {
+            this.mana = mana;
+        }
+       public int mana;
+    }
+}
---
+++ Lesson 28 Inheritance part3 L27cd/Lesson 28 Inheritance part3 L27cd/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Lesson_28_Inheritance_part3_L27cd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Mage m1 = new Mage("Elf", 150, 300);
+
+            m1.Movement();
+            Console.WriteLine(m1.name + " " + m1.HealthPoints + " " + m1.mana);
+
+            Console.ReadKey();
+
+        }
+    }
+    abstract class Character
+    {
+        //ctor + tab tab  utworzeniekonstruktora .
+        //public Character()
+        //{
+
+        //}
+        public Character(string name, int HealthPoints)
+        {
+            this.name = name;
+            this.HealthPoints = HealthPoints;
+        }
+        public abstract void Movement();
+        //{
+        //    Console.WriteLine("Character Movement");
+        //}
+        public string name;
+        public int HealthPoints;
+
+    }
+    // po sealed nie mozna dziedziczyc
+    sealed class Mage : Character
+    {
+        public Mage(string name, int HealthPoints, int mana) /*:base(name,HealthPoints)*/ : base("Elf", 150)
+        //base metoda by zdobyc zmienne dziedziczone po wypisaniu 
+        {
+            this.mana = mana;
+        }
+        public int mana;
+
+        public override void Movement()
+        {
+            Console.WriteLine("Mage Movement");
+            // base(Movement()); zapytac aleksa  czemu w apbstractyjnej nie mozna .
+        }
+        //public override void Movement()
+        //{
+
+        //    Console.WriteLine("Mage Movement");
+        // //   base.Movement();
+        //}
+
+    }
+    //class SuperMage : Mage
+    //{
+
+    //}
+}
