8d6e9112ae94756bf90f3b0da16fab6110487acc
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-21 05:40:02 +0200

    interface review c#

---
+++ Lesson 29 Interfaces/Lesson 29 Interfaces/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lesson_29_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Character p1 = new Character("Elf", 200);
+            IMagicAttack attack = (IMagicAttack)p1;
+            attack.Attack(25);
+            IPhysicalAttack attack2 = (IPhysicalAttack)p1;
+            attack2.Attack(50);
+            Console.ReadKey();
+        }
+
+        interface IMagicAttack
+        {
+            void Attack(int pkt); 
+        }
+        interface IPhysicalAttack
+        {
+            void Attack(int pkt);
+        }
+
+        class Character : IMagicAttack,IPhysicalAttack
+        {
+            public Character(string name, int HealthPoints)
+            {
+                this.name = name;
+                this.HealthPoints = HealthPoints;
+            }
+            public string name;
+            public int HealthPoints;
+
+            void IMagicAttack.Attack(int pkt)
+            {
+                Console.WriteLine("Imagic attack");            }
+
+            void IPhysicalAttack.Attack(int pkt)
+            {
+                Console.WriteLine("Iattack phycical");            }
+        }
+    }
+}
