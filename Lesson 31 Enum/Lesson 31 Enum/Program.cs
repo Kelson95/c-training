04cee74bd4020091b646ac4ccf131172bf41f79e
Kelson95 <109625973+Kelson95@users.noreply.github.com>
2022-07-21 05:41:44 +0200

    Enum review c#

---
+++ Lesson 31 Enum/Lesson 31 Enum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson_31_Enum
+{
+
+    enum Seasons : sbyte// typ wyliczeniowy
+    {
+        Spring = 15, Summer = 25, Autumn = 15,Winter = -10 // convert to string itself
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //string Season = "Spring";
+            //string Season2 = "Summer";
+
+            Seasons Season = Seasons.Winter;
+            Console.WriteLine(Seasons.Winter);
+            Console.WriteLine((int)Season);
+            Seasons Season2 = (Seasons)25;
+            Console.WriteLine(Season2);
+
+        }
+    }
+}
